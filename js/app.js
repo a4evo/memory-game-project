764aa6d25c541dec4b5746260f56eb44c3eb64f6
Anastasiya Shtrakh <a4evo@yandex.ru>
2018-06-15 18:18:55 +0300

    feat: use more const and less let

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * Create a list that holds all of your cards and other common variables
  */
 
-let cardsList = ["fa-diamond",
+const cardsList = ["fa-diamond",
                  "fa-paper-plane-o",
                  "fa-anchor",
                  "fa-bolt",
@@ -17,17 +17,19 @@ let cardsList = ["fa-diamond",
                  "fa-cube",
                  "fa-leaf",
                  "fa-bicycle",
-                 "fa-bomb"],
+                 "fa-bomb"];
 
-    openedCards = 0,
+let openedCards = 0,
     firstCardOpened = "",
     blockOpening = false,
     movesDone = 0,
     restartBtnActive = false,
     leftUnmatched,
-    timeLeft = {h: 0,
-               min: 0,
-               sec: 0};
+    timeLeft = {
+        h: 0,
+        min: 0,
+        sec: 0
+    };
 
 //wait while document ready
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -62,13 +64,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 //Function creating deck
 function createDeck() {
 
-    let shuffledCards = shuffle(cardsList);
+    const shuffledCards = shuffle(cardsList);
 
     const newDeck = document.createElement("ul");
     newDeck.classList.add("deck");
 
-    for (let card of shuffledCards) {
-        let newCard = `<li class="card"><i class="fa ${card}"></i></li>`;
+    for (const card of shuffledCards) {
+        const newCard = `<li class="card"><i class="fa ${card}"></i></li>`;
         newDeck.insertAdjacentHTML('beforeend', newCard);
     }
 
@@ -83,19 +85,21 @@ function createDeck() {
 
     leftUnmatched = 8;
 
-    document.querySelector(".deck").addEventListener("click", function(event) {
+    document.querySelector(".deck").addEventListener("click", function (event) {
         cardClick(event);
     });
-    
-    timeLeft = {h: 0,
-               min: 0,
-               sec: 0};
-    setIntervalID = setInterval(function() {
-        timeLeft.sec ++;
+
+    timeLeft = {
+        h: 0,
+        min: 0,
+        sec: 0
+    };
+    setIntervalID = setInterval(function () {
+        timeLeft.sec++;
         updateTimer();
     }, 1000);
-    
-    
+
+
 }
 
 function cardClick(event) {
@@ -147,132 +151,136 @@ function cardClick(event) {
     }
 }
 
-    //function toggling classes from array
-    function toggleClasses(target, classes = []) {
-        for (let className of classes) {
-            target.classList.toggle(className);
-        }
+//function toggling classes from array
+function toggleClasses(target, classes = []) {
+    for (const className of classes) {
+        target.classList.toggle(className);
+    }
+}
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
 
-    // Shuffle function from http://stackoverflow.com/a/2450976
-    function shuffle(array) {
-        var currentIndex = array.length,
-            temporaryValue, randomIndex;
+    return array;
+}
 
-        while (currentIndex !== 0) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
+function updateMovesNumber() {
+    document.querySelector(".moves").textContent = movesDone;
 
-        return array;
-    }
+    const stars = document.querySelector(".stars").children;
 
-    function updateMovesNumber() {
-        document.querySelector(".moves").textContent = movesDone;
-
-        const stars = document.querySelector(".stars").children;
-
-        switch (movesDone) {
-            case 0:
-                for (const star of stars) {
-                    star.classList.remove("lost-star");
-                }
-                break;
-            case 11:
-                stars[2].classList.add("lost-star");
-                break;
-            case 21:
-                stars[1].classList.add("lost-star");
-                break;
-            case 31:
-                stars[0].classList.add("lost-star");
-                break;
-        }
-    }
-
-    //Create pop-up window with message and buttons
-
-    function showPopup(message, buttons, action) {
-
-        let newPopup = document.createElement("div");
-        newPopup.classList.add("popup");
-
-        let innerContainer = document.createElement("div");
-        innerContainer.classList.add("popup-inner");
-
-        let title = document.createElement("h1");
-        title.insertAdjacentHTML('afterbegin', message);
-        innerContainer.appendChild(title);
-
-        if (action === "finish") {
-
-            const starsRating = document.querySelector(".stars").cloneNode(true);
-            innerContainer.appendChild(starsRating);
-            
-            const timerDiv = document.createElement("div");
-            const timerSpan = document.querySelector(".timer").cloneNode(true);
-            timerSpan.insertAdjacentHTML('afterbegin', "<span>Your time: </span>");
-            timerDiv.appendChild(timerSpan);
-            innerContainer.appendChild(timerDiv);
-            
-        }
-
-        for (const button of buttons) {
-            let newButton = document.createElement("button");
-            newButton.classList.add("btn");
-            newButton.insertAdjacentHTML("afterbegin", button);
-            innerContainer.appendChild(newButton);
-        }
-
-        newPopup.appendChild(innerContainer);
-
-
-        document.querySelector("body").appendChild(newPopup);
-
-        //add event listeners for buttons
-        const btns = document.querySelectorAll(".btn");
-
-        for (const btn of btns) {
-            addEventListenersToBtns(btn, action);
-        }
-    }
-
-    function addEventListenersToBtns(btn, action) {
-
-        btn.addEventListener("click", function (event) {
-
-            const userAnswer = event.target.textContent;
-            console.log(userAnswer);
-            if (userAnswer !== "no") {
-                document.querySelector('.deck').remove();
-                createDeck();
+    switch (movesDone) {
+        case 0:
+            for (const star of stars) {
+                star.classList.remove("lost-star");
             }
-
-            document.querySelector(".popup").remove();
-        });
+            break;
+        case 11:
+            stars[2].classList.add("lost-star");
+            break;
+        case 21:
+            stars[1].classList.add("lost-star");
+            break;
+        case 31:
+            stars[0].classList.add("lost-star");
+            break;
     }
+}
+
+//Create pop-up window with message and buttons
+
+function showPopup(message, buttons, action) {
+
+    let newPopup = document.createElement("div");
+    newPopup.classList.add("popup");
+
+    let innerContainer = document.createElement("div");
+    innerContainer.classList.add("popup-inner");
+
+    let title = document.createElement("h1");
+    title.insertAdjacentHTML('afterbegin', message);
+    innerContainer.appendChild(title);
+
+    if (action === "finish") {
+
+        const starsRating = document.querySelector(".stars").cloneNode(true);
+        innerContainer.appendChild(starsRating);
+
+        const timerDiv = document.createElement("div");
+        const timerSpan = document.querySelector(".timer").cloneNode(true);
+        timerSpan.insertAdjacentHTML('afterbegin', "<span>Your time: </span>");
+        timerDiv.appendChild(timerSpan);
+        innerContainer.appendChild(timerDiv);
+
+    }
+
+    for (const button of buttons) {
+        let newButton = document.createElement("button");
+        newButton.classList.add("btn");
+        newButton.insertAdjacentHTML("afterbegin", button);
+        innerContainer.appendChild(newButton);
+    }
+
+    newPopup.appendChild(innerContainer);
+
+
+    document.querySelector("body").appendChild(newPopup);
+
+    //add event listeners for buttons
+    const btns = document.querySelectorAll(".btn");
+
+    for (const btn of btns) {
+        addEventListenersToBtns(btn, action);
+    }
+}
+
+function addEventListenersToBtns(btn, action) {
+
+    btn.addEventListener("click", function (event) {
+
+        const userAnswer = event.target.textContent;
+        console.log(userAnswer);
+        if (userAnswer !== "no") {
+            document.querySelector('.deck').remove();
+            createDeck();
+        }
+
+        document.querySelector(".popup").remove();
+    });
+}
 
 function updateTimer() {
-   
+
     if (timeLeft.sec == 60) {
         timeLeft.sec = 0;
-        timeLeft.min ++;            
+        timeLeft.min++;
     }
     if (timeLeft.min == 60) {
         timeLeft.min = 0;
-        timeLeft.h ++;
+        timeLeft.h++;
     }
-    
+
     const timer = document.querySelector(".timer");
-    
-    let {sec, min, h} = timeLeft;
-    
+
+    let {
+        sec,
+        min,
+        h
+    } = timeLeft;
+
     sec = (sec < 10) ? ("0" + sec) : (sec);
     min = (min < 10) ? ("0" + min) : (min);
-    h  = (h < 10) ? ("0" + h) : (h);
-    
+    h = (h < 10) ? ("0" + h) : (h);
+
     timer.textContent = `${h}:${min}:${sec}`;
 }
